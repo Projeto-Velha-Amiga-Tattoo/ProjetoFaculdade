5e3b7bf4de6953c1c852b24ddf96ec23c2304c3e
Luan <projetodeextensaogithub@gmail.com>
2023-10-17 12:17:29 -0300

    feitos alguns testes

--- src/main/java/Objects/VelhaAmiga.java
+++ src/main/java/Objects/VelhaAmiga.java
@@ -2,6 +2,11 @@ package Objects;
 
 public class VelhaAmiga {
     public static void main(String[] args) {
+      Endereco e1 = new Endereco(1,"Rua Serra xxxx xxxxxx","Jardim xxxxx",0,"casa","Ipaxxxx","3516xxxx","Minas Gerais");
+      Funcionario f1 = new Funcionario(1,"luan","12344","tatuador","Luan Vixxx xxxxxx","155xxxxxx40","3199751xxxx","xx/xx/2000",e1);  
+      System.out.println(f1.getEndereco().getRua());
+      System.out.println(f1.getEndereco().getCidade());
+        
         
     }
 }
